61aaa4b469744a0990fc50046aa958fde5693433
liangzhengtao.lzt <liangzhengtao.lzt@lazada.com>
2020-12-26 11:54:05 +0800

    [feat] 依赖注入之set注入

--- spring-study/spring-01-ioc/src/main/java/com/example/pojo/Student.java
+++ spring-study/spring-01-ioc/src/main/java/com/example/pojo/Student.java
@@ -86,7 +86,7 @@ public class Student {
     public String toString() {
         return "Student{" +
                 "name='" + name + '\'' +
-                ", address=" + address +
+                ", address=" + address.toString() +
                 ", books=" + Arrays.toString(books) +
                 ", hobby=" + hobby +
                 ", card=" + card +
--- spring-study/spring-01-ioc/src/test/java/MyTest.java
+++ spring-study/spring-01-ioc/src/test/java/MyTest.java
@@ -12,6 +12,6 @@ public class MyTest {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         Student student = (Student) context.getBean("student");
-        System.out.println(student.getName());
+        System.out.println(student.toString());
     }
 }
